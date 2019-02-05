a97925db0a8dabfee6810163468cf653fdfefd91
DIR\felipe.tofoli <felipe.tofoli@concrete.com.br>
2019-02-05 19:44:31 -0200

    Adding swagger, swaggerUi.

--- src/Resilient.WebApi/Controllers/PostsController.cs
+++ src/Resilient.WebApi/Controllers/PostsController.cs
@@ -1,10 +1,13 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Resilient.WebApi.Client;
 using Resilient.WebApi.Models;
+using System.Collections.Generic;
+using System.Net;
 using System.Threading.Tasks;
 
 namespace Resilient.WebApi.Controllers
 {
+    [Produces("application/json")]
     [Route("api/[controller]")]
     [ApiController]
     public class PostsController : ControllerBase
@@ -13,27 +16,52 @@ namespace Resilient.WebApi.Controllers
 
         public PostsController(PostClient jsonPlaceHolderClient)
         {
-            this.postClient = jsonPlaceHolderClient;
+            postClient = jsonPlaceHolderClient;
         }
 
-        // GET api/values
+        /// <summary>
+        /// Gets all blog posts.
+        /// </summary>
+        /// <returns>All blog posts.</returns>
         [HttpGet]
+        [ProducesResponseType(typeof(IEnumerable<PostModel>), (int)HttpStatusCode.OK)]
         public async Task<IActionResult> Get()
         {
             var repositories = await postClient.GetPosts();
             return Ok(repositories);
         }
 
-        // GET api/values/5
+        /// <summary>
+        /// Gets the specified blog post.
+        /// </summary>
+        /// <param name="id">The id of the blog post desired.</param>
+        /// <returns>A specified blog post.</returns>
         [HttpGet("{id}")]
+        [ProducesResponseType(typeof(PostModel), (int)HttpStatusCode.OK)]
         public async Task<IActionResult> Get(int id)
         {
             var post = await postClient.GetPost(id);
             return Ok(post);
         }
 
-        // POST api/values
+        /// <summary>
+        /// Creates a new blog post.
+        /// </summary>
+        /// <remarks>
+        /// Sample request:
+        ///
+        ///     POST /posts
+        ///     {
+        ///        "userId": 1,
+        ///        "title": "title name",
+        ///        "body": "body text"
+        ///     }
+        ///
+        /// </remarks>
+        /// <param name="value"></param>
+        /// <returns>Status of success.</returns>
         [HttpPost]
+        [ProducesResponseType((int)HttpStatusCode.OK)]
         public async Task<IActionResult> Post([FromBody] PostModel value)
         {
             await postClient.BlogPost(value);
--- src/Resilient.WebApi/Startup.cs
+++ src/Resilient.WebApi/Startup.cs
@@ -5,8 +5,11 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Polly;
 using Resilient.WebApi.Client;
+using Swashbuckle.AspNetCore.Swagger;
 using System;
+using System.IO;
 using System.Net.Http;
+using System.Reflection;
 
 namespace Resilient.WebApi
 {
@@ -33,11 +36,31 @@ namespace Resilient.WebApi
                     .AddTransientHttpErrorPolicy(policy => policy.RetryAsync(3))
                     .AddTransientHttpErrorPolicy(policy => policy.CircuitBreakerAsync(5, TimeSpan.FromSeconds(30)))
                     .AddPolicyHandler(Policy.TimeoutAsync<HttpResponseMessage>(30));
+
+            // Register the Swagger generator.
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "Posts API", Version = "v1" });
+
+                // Set the comments path for the Swagger JSON and UI.
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Posts API V1");
+            });
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
